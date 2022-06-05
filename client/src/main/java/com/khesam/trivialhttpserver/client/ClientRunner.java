f2f5c02f75e448ae57270592144a5b4f0aef08e3
khesam109 <khesam109@gmail.com>
2022-06-05 04:20:11 -0700

    the project has been created!

---
+++ client/src/main/java/com/khesam/trivialhttpserver/client/ClientRunner.java
@@ -0,0 +1,8 @@
+package com.khesam.trivialhttpserver.client;
+
+public class ClientRunner {
+
+    public static void main(String[] args) {
+        System.out.println("Client has been started!");
+    }
+}
---
+++ server/src/main/java/com/khesam/trivialhttpserver/server/ServerRunner.java
@@ -0,0 +1,8 @@
+package com.khesam.trivialhttpserver.server;
+
+public class ServerRunner {
+
+    public static void main(String[] args) {
+        System.out.println("The server has been started!");
+    }
+}
